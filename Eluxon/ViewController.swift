31017c25f9276dae0544ea57f789af93678cb714
Himmat Singh <himmat.sidhu521@gmail.com>
2020-11-05 23:05:11 -0800

    Final Variation

--- Eluxon/ViewController.swift
+++ Eluxon/ViewController.swift
@@ -19,7 +19,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toggleBtnWasPressed(_ sender: Any) {
+        switchStatus.toggle()
+        if switchStatus == .off{
+            toggleBtn.setImage(UIImage(named: "offBtn"), for: .normal)
+            view.backgroundColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+            onOffLbl.text = "🌑 OFF 🌑"
+            onOffLbl.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }else{
+            toggleBtn.setImage(UIImage(named: "onBtn"), for: .normal)
+            view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            onOffLbl.text = "☀️ ON ☀️"
+            onOffLbl.textColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+        }
+    }
     
 }
 
